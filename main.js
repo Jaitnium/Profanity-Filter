f7b6c3a28185e8fbf9d34405843262b05b0bb62b
Matthew <matthewaantosiak@gmail.com>
2022-09-22 18:27:47 -0700

    Final presentation version

--- main.js
+++ main.js
@@ -4,31 +4,6 @@
        // create a new string without any naughty words
        // update that text field
 
-//const filter = import{'./filter'};
-//import { filter } from './filter.js';
-//censor = require('./filter.js');
-/*
-const avoid = {
-  'BODY' : true,
-  'STYLE' : true,
-  'SCRIPT' : true,
-  'LINK' : true,
-  'HEAD' : true,
-  'PATH' : true,
-  'SVG' : true,
-  'G' : true,
-  'A' : true,
-  'HTML' : true,
-  'META' : true,
-  'IFRAME' : true,
-  'IMG' : true,
-  'BR' : true
-};*/
-
-// p, span, h1, h2, h3, h4, h5, h6, title, main, nav, section
-
-
-
 function traverseDOM() {
 
   /*
@@ -36,7 +11,7 @@ function traverseDOM() {
   console.log('traverseDom_target:', target);*/
   const allComments = document.getElementsByClassName('_1qeIAgB0cPwnLhDF9XSiJM');
 
-  console.log(allComments);
+  //console.log(allComments);
 
   console.log('totalComments:', allComments.length);
 
@@ -47,91 +22,55 @@ function traverseDOM() {
     //console.log(allElements[i].getAttribute('name'));
     console.log(comment.nodeName, typeof comment.innerText, comment.innerText);
     //console.log('before filtering', currElement.innerText);
-    comment.innerText = filter(comment.innerText);
+    filter(comment);
+    console.log('after_filter:', comment);
         //console.log('after filtering', currElement.innerText);
   }
-  /*
-  //document.body.style.backgroundColor = 'red';
-  const allElements = document.getElementsByTagName('*');
-  for (let i = 0; i < 1000; i++) {
-    try {
-      // For the current element
-      let currElement = allElements[i];
-      // If it's not an element that we want to avoid
-      if (currElement && !avoid[currElement.nodeName.toUpperCase()]) {
-        //console.log(allElements[i].getAttribute('name'));
-        console.log(currElement.nodeName, typeof currElement.innerText, currElement.innerText);
-        //console.log('before filtering', currElement.innerText);
-        currElement.textContent = filter(currElement.textContent);
-        //console.log('after filtering', currElement.innerText);
-      }
-    }
-    catch (err) {
-      console.log(err);
-    }
-
-  }*/
 }
 
-// CAUTION: NAUGHTY WORDS
-const badWords =
-{
-  'motherfucker': '*firetrucker*',
-  'motherfucka': '*firetrucka*',
-  'bitch': '*witch*',
-  'cum': '*dumb*',
-  'ass': '*donkey*',
-  'fuck': '*duck*',
-  'crap': '*poo*',
-  'shit': '*poop*',
-  'dick': '*snitch*',
-  'cock' : '*rooster*',
-  'bastard' : '*custard*',
-  'cunt' : '*bunt*'
-};
+const naughtyRegex = /motherfucker|dipshit|motherfucka|fucker|fucka|bitch|cum|ass|fuck|crap|shit|dick|cock|bastard|cunt|whore|damn|prick|douche|nigger|faggot|tit|titty|titties|piss|turd|twat|slut|sperm|wench|trash|anal|retard|retarded|hentai|hentia|porn|vagina|racist|god|dammit|damnit|nipples|jesus|fister|humping|hump|pedophile|pedo|paedo|shart|nigga/ig;
 
-function filter(str) {
+function getNiceWord() {
+  const niceWords = ['salt', 'bubble', 'friend', 'butter', 'guy', 'rooster', 'custard', 'firetruck', 'muffin', 'duck', 'donkey', 'pillow', 'blanket', 'gosh', 'darn', 'food', 'walrus', 'unicorn', 'wizard', 'rainbow', 'sailor', 'cat', 'athelete'];
+  console.log(Math.floor(Math.random() * niceWords.length));
+  return niceWords[Math.floor(Math.random() * niceWords.length)];
+}
 
-  console.log('filter??');
-  // handle case sensitive
-  for (const key in badWords) {
-    str = str.replaceAll(key, badWords[key]);
+function filter(commentEle) {
+  console.log('filter_input:', commentEle);
+  try {
+    commentEle.innerHTML = commentEle.textContent.replaceAll(naughtyRegex, getNiceWord);
   }
-
-  return str;
+  catch (err) {
+    console.log(err);
+    return;
+  }
 }
 
-traverseDOM();
+//traverseDOM();
+
+
+function eventFire(event) {
+  console.log('eventFired:', event.type);
+}
 
 document.addEventListener('scroll', (event) => {
+  eventFire(event);
+  traverseDOM();}
+);
+
+window.onload = (event) => {
+  console.log('page is fully loaded');
+  traverseDOM();
+};
+document.addEventListener('DOMContentLoaded', (event) => eventFire(event));
+
+document.addEventListener('load', (event) => {
+
+  eventFire(event);
   console.log('target:', event.target);
   console.log('first_child:', event.target.firstChild);
 
   traverseDOM();
-  // const allComments = event.target.getElementsByClassName('_1qeIAgB0cPwnLhDF9XSiJM');
-  // console.log(allComments, typeof(allComments));
-  // //allComments.forEach((element => element.innerText = element.innerText + 's'));
-  // for(let i = 0; i < allComments.length; i++) {
-  //   allComments[i].innerText = filter(allComments[i].innerText); // + 's';
-  // }
-  /*
-  traverseDOM(event.target);*/
-  /*
-  setTimeout(() => traverseDOM(), 3000)
- */
-  console.log(event)});
-
-
-// async helperFunction() {
-//   await traverseDOM();
-// }
-
-
-// console.log(chrome);
-// console.log(chrome.action);
-// console.log(chrome.browserAction);
-
-/*
-chrome.browserAction.onClicked.addListener((tabs) => {
-  console.log('hi');
-});*/
+  console.log(event);
+});
